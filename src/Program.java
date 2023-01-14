2c379165d7aa21b4e7f9cbe94eb15e363ed12e13
DudaCampello <114358060+DudaCampello@users.noreply.github.com>
2023-01-14 08:10:04 -0300

    first commit

---
+++ src/Program.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        String arquivoDoPc = leitor.next();
+
+        if (arquivoDoPc.contains("mp3")) {
+            System.out.println("Salvar");
+        } else {
+            System.out.println("Deletar");
+        }
+    }
+}
+
+
+
